626107e0bf118200ac762ebe365642d10d2a2bd2
Vr61v <nikita20040503@gmail.com>
2024-08-14 08:56:49 +0300

    initial project

---
+++ src/main/java/com/vr61v/SpringShoppingBot/SpringShoppingBotApplication.java
@@ -0,0 +1,13 @@
+package com.vr61v.SpringShoppingBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShoppingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShoppingBotApplication.class, args);
+	}
+
+}
